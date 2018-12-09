b1cf003490a2d7bfbfb44f1a7761ab937a4f1666
Zeva-Notebook <zevadrows@gmail.com>
2018-12-09 20:01:40 -0200

    primeiro commit

---
+++ src/entities/Convidado.java
@@ -0,0 +1,42 @@
+package entities;
+
+import entities.enums.Alimentacao;
+
+public class Convidado {
+	
+	private String nome;
+	private String bebida;
+	private Alimentacao aliment;
+	
+	public Convidado(String nome, boolean bebida, Alimentacao aliment) {
+		this.nome = nome;
+		this.bebida = bebida;
+		this.aliment = aliment;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getBebida() {
+		return bebida;
+	}
+
+	public void setBebida(String bebida) {
+		this.bebida = bebida;
+	}
+
+	public Alimentacao getAliment() {
+		return aliment;
+	}
+
+	public void setAliment(Alimentacao aliment) {
+		this.aliment = aliment;
+	}
+	
+
+}
